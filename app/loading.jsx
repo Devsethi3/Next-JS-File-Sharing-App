1ac12f10bb38cbd8ee80e36da4ffc523c60b5ae9
Devsethi3 <devsethi4522@gmail.com>
2024-02-12 15:20:44 +0530

    first commit

---
+++ app/loading.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div>
+      <div class="banter-loader">
+        <div class="banter-loader__box"></div>
+        <div class="banter-loader__box"></div>
+        <div class="banter-loader__box"></div>
+        <div class="banter-loader__box"></div>
+        <div class="banter-loader__box"></div>
+        <div class="banter-loader__box"></div>
+        <div class="banter-loader__box"></div>
+        <div class="banter-loader__box"></div>
+        <div class="banter-loader__box"></div>
+      </div>
+    </div>
+  );
+};
+
+export default loading;
